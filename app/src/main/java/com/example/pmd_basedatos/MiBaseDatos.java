dac2797f31c2b6524c257f86d6e0df56fe18e135
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2023-02-20 08:49:19 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pmd_basedatos/MiBaseDatos.java
@@ -0,0 +1,27 @@
+package com.example.pmd_basedatos;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MiBaseDatos extends SQLiteOpenHelper {
+
+    String sqlCreate = "CREATE TABLE pruebas(codigo INTEGER, nombre TEXT)";
+
+    public MiBaseDatos(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(sqlCreate);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS pruebas");
+        sqLiteDatabase.execSQL(sqlCreate);
+    }
+}
